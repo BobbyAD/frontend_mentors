73ca99f8289e0eb19fc4dcb06770cf5b0144cbc2
Robert Driskell <driskell.ba@gmail.com>
2020-08-26 19:19:54 -0500

    Overview done. A little off on the alignment, but I'm not worried about it

--- social-media-dashboard/src/components/overview/Overview.js
+++ social-media-dashboard/src/components/overview/Overview.js
@@ -1,6 +1,8 @@
 import React from "react";
 import OverviewCard from "./OverviewCard";
 
+import styles from "../../styles/modules/overview.module.scss";
+
 const Overview = () => {
     const platforms = ["facebook", "instagram", "twitter", "youtube"];
     const content = [
@@ -12,12 +14,12 @@ const Overview = () => {
         {
             title: "Likes",
             amount: "52",
-            change: "2",
+            change: "-2",
         },
         {
             title: "Likes",
             amount: "5462",
-            change: "3",
+            change: "2257",
         },
         {
             title: "Profile Views",
@@ -37,20 +39,30 @@ const Overview = () => {
         {
             title: "Likes",
             amount: "107",
-            change: "19",
+            change: "-19",
         },
         {
             title: "Total Views",
             amount: "1407",
-            change: "12",
+            change: "-12",
         },
     ];
     return (
-    <div>
-        <h2>Overview - Today</h2>
-        <OverviewCard />
-    </div>
-    )
+        <div className={styles.container}>
+            <h2 className={styles.title}>Overview - Today</h2>
+            <div className={styles.cardContainer}>
+                {platforms.map((p, i) => (
+                    <>
+                        <OverviewCard platform={p} content={content[i * 2]} />
+                        <OverviewCard
+                            platform={p}
+                            content={content[i * 2 + 1]}
+                        />
+                    </>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Overview;
--- social-media-dashboard/src/components/overview/OverviewCard.js
+++ social-media-dashboard/src/components/overview/OverviewCard.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
 
-const OverviewCard = ({platform, content}) => {
+import fb from "../../images/icon-facebook.svg";
+import tw from "../../images/icon-twitter.svg";
+import ig from "../../images/icon-instagram.svg";
+import yt from "../../images/icon-youtube.svg";
+import up from "../../images/icon-up.svg";
+import down from "../../images/icon-down.svg";
+
+import styles from "../../styles/modules/overview.module.scss";
+
+const OverviewCard = ({ platform, content }) => {
     return (
-        <div>
-            
+        <div className={styles.card}>
+            <h4>{content.title}</h4>
+            <PlatformImage platform={platform} />
+            <strong>{content.amount}</strong>
+            <p
+                className={
+                    (content.change > 0 ? styles.positive : styles.negative)
+                }
+            >
+                {content.change > 0 ? <img src={up} /> : <img src={down} />}
+                {content.change}%
+            </p>
         </div>
-    )
-}
+    );
+};
 
-export default OverviewCard
+const PlatformImage = ({ platform }) => {
+    switch (platform) {
+        case "facebook":
+            return (
+                <>
+                    <img src={fb} />
+                </>
+            );
+        case "twitter":
+            return (
+                <>
+                    <img src={tw} />
+                </>
+            );
+        case "instagram":
+            return (
+                <>
+                    <img src={ig} />
+                </>
+            );
+        case "youtube":
+            return (
+                <>
+                    <img src={yt} />
+                </>
+            );
+    }
+
+    return <></>;
+};
+
+export default OverviewCard;
