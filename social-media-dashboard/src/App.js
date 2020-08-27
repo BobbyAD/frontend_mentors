f260d716e5a65912c0b92f88a1f6a03f65932cc9
Robert Driskell <driskell.ba@gmail.com>
2020-08-26 22:13:06 -0500

    sm-dash darkmode test created

--- social-media-dashboard/src/App.js
+++ social-media-dashboard/src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // styles
-import "./styles/theme.scss"
-import "./styles/app.scss"
+import "./styles/theme.scss";
+import "./styles/app.scss";
 
 // components
 import Header from "./components/header/Header";
@@ -10,10 +10,40 @@ import Followers from "./components/followers/Followers";
 import Overview from "./components/overview/Overview";
 
 function App() {
+    const [theme, setTheme] = useState("light");
+    const [transition, setTransition] = useState(false);
+
+    useEffect(() => {
+        const lsTheme = localStorage.getItem("theme");
+        if (lsTheme) {
+            setTheme(lsTheme);
+        }
+    }, []);
+
+    const changeTheme = (theme) => {
+        setTheme(theme + " transition")
+        setTransition(true)
+        localStorage.setItem("theme", theme);
+        setTimeout(() => {
+            setTheme(theme);
+            setTransition(false)
+        }, 1000)
+    };
+
+    const handleClick = () => {
+        if (theme === "light") {
+            changeTheme("dark");
+        } else {
+            changeTheme("light");
+        }
+        console.log(theme)
+    };
+
     return (
-        <div className="App">
+        <div className={"App" + " " + theme}>
             <div className="top-background" />
             <div className="main-container">
+                <button onClick={handleClick} disabled={transition}>asdf</button>
                 <Header />
                 <Followers />
                 <Overview />
